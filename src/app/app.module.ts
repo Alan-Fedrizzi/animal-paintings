a93b18e85aeaaf1e95814afcee46f97f7f210daf
alanfedrizzi <alanfedrizzi@gmail.com>
2024-06-06 12:34:29 +0200

    cart init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ProductModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ProductModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { ProductListComponent } from './product-list/product-list.component';
 
-
-
 @NgModule({
-  declarations: [
-    ProductListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProductListComponent],
+  imports: [CommonModule, MatCardModule, FlexLayoutModule],
 })
-export class ProductModule { }
+export class ProductModule {}
